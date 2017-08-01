4381125e497c69459f56fc32d4940a249ab410d7
vvildvvildv <vvildvvildv@gmail.com>
2017-08-01 21:08:23 +0200

    last files to be added to git before develpment

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+// prints e.g. 'Current PHP version: 4.1.1'
+//echo 'Current PHP version: ' . phpversion();
+$version = phpversion();
+echo "Current PHP version is $version";
+// prints e.g. '2.0' or nothing if the extension isn't enabled
+//echo ("<br>");
+//echo phpversion('tidy');
+?>
